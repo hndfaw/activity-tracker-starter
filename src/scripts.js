b97bd32aecb0ca0b471347cbaaaba5ede7378056
“Hindreen” <hndfaw@gmail.com>
2019-05-11 08:51:42 -0600

    Add first iteration to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,13 @@
 $(window).on('load', () => {
-    const randomNum = 1 + Math.floor(Math.random() * userData.length)
-    const userRepository = new UserRepository(randomNum);
+    const randomID = Math.floor(Math.random() * userData.length) + 1
+    const userRepository = new UserRepository(randomID);
     $('.aside__welcome-name').html(userRepository.userFirstName());
     $('.aside__fullname').html(userRepository.userData().name);
     $('.aside__address').html(userRepository.userData().address);
     $('.aside__email').html(userRepository.userData().email);
     $('.aside__stride-length').html(userRepository.userData().strideLength);
+    $('.your-step-goal').html(userRepository.userData().dailyStepGoal);
+    $('.others-step-goal').html(userRepository.averageStepGoal());
+    console.log()
 })
 
