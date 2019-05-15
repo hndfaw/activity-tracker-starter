0156a18ad16817b59ecc923b912fcba6c7c99c9f
“Hindreen” <hndfaw@gmail.com>
2019-05-15 12:21:11 -0600

    Fix activity links

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,9 +1,8 @@
-if (typeof module !== 'undefined') {
-
- hydration = require('../data/hydrationSub.js');
- 
+var hydration
+if (typeof module !== "undefined") {
+  hydration = require("../data/hydrationSub.js");
 } else {
-  hydration = hydrationData
+  hydration = hydrationData;
 }
 
 class Hydration {
@@ -13,16 +12,19 @@ class Hydration {
 
   userData() {
     return hydration.find(element => element.userID === this.userID);
-  } 
- 
+  }
+
   getAvgFluidConsumption() {
-    var ozsConsumed = this.userData().hydrationData.reduce((acc, cur) =>
-    acc + cur.numOunces,0)/this.userData().hydrationData.length;
+    var ozsConsumed =
+      this.userData().hydrationData.reduce(
+        (acc, cur) => acc + cur.numOunces,
+        0
+      ) / this.userData().hydrationData.length;
     return parseFloat(ozsConsumed.toFixed(1));
   }
 
   ozsConsumedOnDate(date) {
-    return this.userData().hydrationData.find(el => el.date == date).numOunces
+    return this.userData().hydrationData.find(el => el.date == date).numOunces;
   }
 
   getWeekFluidConsumption(date) {
@@ -30,25 +32,25 @@ class Hydration {
     var userFluidData = this.userData().hydrationData;
     var firstDay = userFluidData.find(el => el.date === date);
     var firstDayIndex = userFluidData.indexOf(firstDay);
-    var fixFirstDayIndex = (firstDayIndex - 7) > 0 ? firstDayIndex - 7 : 0;
+    var fixFirstDayIndex = firstDayIndex - 7 > 0 ? firstDayIndex - 7 : 0;
     let fluidData = userFluidData.slice(fixFirstDayIndex, firstDayIndex);
     for (let i = 0; i < fluidData.length; i++) {
-      fluidWeekDays.push(fluidData[i].numOunces )
-      }      
+      fluidWeekDays.push(fluidData[i].numOunces);
+    }
     var finalFluidWeekDays = [];
     if (fluidWeekDays.length === 7) {
-      finalFluidWeekDays = fluidWeekDays
+      finalFluidWeekDays = fluidWeekDays;
     } else {
       var newArray = [];
-      for(var i = 0; i < 7 - fluidWeekDays.length; i++) {
-        newArray.push('-')
+      for (var i = 0; i < 7 - fluidWeekDays.length; i++) {
+        newArray.push("-");
       }
-      finalFluidWeekDays = newArray.concat(fluidWeekDays)
+      finalFluidWeekDays = newArray.concat(fluidWeekDays);
     }
-    return finalFluidWeekDays
+    return finalFluidWeekDays;
   }
 }
 
 if (typeof module !== "undefined") {
   module.exports = Hydration;
- }
+}
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,30 +1,37 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const Hydration = require('../src/Hydration');
+const Hydration = require("../src/Hydration");
 
-describe('Hydration', function() {
-  let hydration
+describe("Hydration", function() {
+  let hydration;
 
   beforeEach(function() {
-     hydration = new Hydration(1);
-
+    hydration = new Hydration(1);
   });
-  it('should be a function', function() {
-    expect(Hydration).to.be.a('function');
+  it("should be a function", function() {
+    expect(Hydration).to.be.a("function");
   });
 
-  it('should be an instance of Hydration', function() {
+  it("should be an instance of Hydration", function() {
     expect(hydration).to.be.an.instanceof(Hydration);
   });
 
-  it('should return a user average ounces of liquid consumed', function() {
+  it("should return a user average ounces of liquid consumed", function() {
     expect(hydration.getAvgFluidConsumption()).to.equal(55.4);
   });
-  it('should return a users liquid consumed on a given date', function() {
-    expect(hydration.ozsConsumedOnDate('07/05/2019')).to.equal(80);
+  it("should return a users liquid consumed on a given date", function() {
+    expect(hydration.ozsConsumedOnDate("07/05/2019")).to.equal(80);
   });
 
-  it('should return the fluid consumption for a week', function() { 
-    expect(hydration.getWeekFluidConsumption('14/05/2019')).to.deep.equal([80, 39, 40, 65, 84, 33, 60]);
-  })
+  it("should return the fluid consumption for a week", function() {
+    expect(hydration.getWeekFluidConsumption("14/05/2019")).to.deep.equal([
+      80,
+      39,
+      40,
+      65,
+      84,
+      33,
+      60
+    ]);
+  });
 });
